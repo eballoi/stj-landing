8182f666db8cf8010a7d9da9c90df9904d1308e0
Edoardo Balloi <edoardo.balloi@gmail.com>
2025-01-14 11:52:15 +0100

    feat: hero section

---
+++ src/app/dictionaries/en.ts
@@ -0,0 +1,24 @@
+export const en = {
+	pages: {
+		appName: "Snipe That Job",
+		landing: {
+			hero: {
+				heading: "It's time to get it!",
+				description:
+					"1. Upload your CV 2. Link you offer 3. Let the magic happen",
+			},
+			feature: {
+				heading: "Feature",
+				description: "all the stufff",
+			},
+			reviews: {
+				heading: "Reviews",
+				description: "all the stufff",
+				items: {
+					name: "Manu",
+					review: "This is the best product ever",
+				},
+			},
+		},
+	},
+};
